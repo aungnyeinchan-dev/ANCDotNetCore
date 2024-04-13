664acc4ec6f2e76e6b192d93abd20f50d4668868
Aung Nyein Chan <axonlang5@gmail.com>
2024-04-14 00:04:45 +0630

    [Complete] Day2 Ado.Net CRUD

--- ANCDotNetCore.ConsoleApp/Program.cs
+++ ANCDotNetCore.ConsoleApp/Program.cs
@@ -1,10 +1,11 @@
-﻿using System.Data;
+﻿using ANCDotNetCore.ConsoleApp;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+/*SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
 stringBuilder.DataSource = "DESKTOP-GCNGMKN";
 stringBuilder.InitialCatalog = "DotNetTraingBatch4";
 stringBuilder.UserID = "sa";
@@ -29,6 +30,13 @@ foreach (DataRow dr in dt.Rows)
     Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
     Console.WriteLine("Blog Content => " + dr["BlogContent"]);
     Console.WriteLine("_____________________________________");
-}
+}*/
 
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(11, "LORD OF THE RINGS", "J.R.R. Tolkien", "Test Content");
+//adoDotNetExample.Delete(12);
+adoDotNetExample.Edit(12);
+adoDotNetExample.Edit(1);
 Console.ReadKey();

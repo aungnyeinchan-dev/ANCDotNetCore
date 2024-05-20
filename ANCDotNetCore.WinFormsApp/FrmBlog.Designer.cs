8dedb09dbf0256ba68f78fa7ac91bb7ef1c980c4
Aung Nyein Chan <axonlang5@gmail.com>
2024-05-20 18:30:45 +0630

    [Complete] Windows Forms Blog - Create

---
+++ ANCDotNetCore.WinFormsApp/FrmBlog.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ANCDotNetCore.WinFormsApp
+{
+    partial class FrmBlog
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtTitle = new TextBox();
+            txtAuthor = new TextBox();
+            txtContent = new TextBox();
+            btnSave = new Button();
+            btnCancel = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Title :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Author :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(28, 107);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Content :";
+            // 
+            // txtTitle
+            // 
+            txtTitle.Location = new Point(28, 37);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(310, 23);
+            txtTitle.TabIndex = 3;
+            // 
+            // txtAuthor
+            // 
+            txtAuthor.Location = new Point(28, 81);
+            txtAuthor.Name = "txtAuthor";
+            txtAuthor.Size = new Size(310, 23);
+            txtAuthor.TabIndex = 4;
+            // 
+            // txtContent
+            // 
+            txtContent.Location = new Point(28, 125);
+            txtContent.Multiline = true;
+            txtContent.Name = "txtContent";
+            txtContent.Size = new Size(310, 78);
+            txtContent.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.FromArgb(67, 160, 71);
+            btnSave.FlatStyle = FlatStyle.Flat;
+            btnSave.ForeColor = Color.White;
+            btnSave.Location = new Point(109, 209);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 28);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "&Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.BackColor = Color.FromArgb(229, 57, 53);
+            btnCancel.FlatStyle = FlatStyle.Flat;
+            btnCancel.ForeColor = Color.White;
+            btnCancel.Location = new Point(28, 209);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 28);
+            btnCancel.TabIndex = 7;
+            btnCancel.Text = "C&ancel";
+            btnCancel.UseVisualStyleBackColor = false;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // FrmBlog
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(534, 261);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(txtContent);
+            Controls.Add(txtAuthor);
+            Controls.Add(txtTitle);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(2);
+            Name = "FrmBlog";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Blog";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtTitle;
+        private TextBox txtAuthor;
+        private TextBox txtContent;
+        private Button btnSave;
+        private Button btnCancel;
+    }
+}
